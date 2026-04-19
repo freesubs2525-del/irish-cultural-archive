67211ec51edbdf533d23b1a0960a513a3cec3b9c
freesubs2525-del <freesubs2525@gmail.com>
2026-04-19 23:26:39 +0200

    Initial Launch
    this is the first version of the database/website

---
+++ county_data.js
@@ -0,0 +1,321 @@
+var countyData = {
+    "Dublin": {
+        count: 1,
+        figures: [{
+            name: "Countess Constance Markievicz",
+            dates: "1868 – 1927",
+            bio: "Irish revolutionary, suffragist, and socialist. She was the first woman elected to the British House of Commons and a leader in the 1916 Easter Rising.",
+            works: ["Prison Letters"],
+            image: "https://d.imgvision.net/irishcreativeproject/unnamed.jpg"
+        }]
+    },
+    "Sligo": {
+        count: 1,
+        figures: [{
+            name: "W.B. Yeats",
+            dates: "1865 – 1939",
+            bio: "The heartbeat of Irish literary nationalism, blending mythology and politics to define Irish identity.",
+            works: ["The Lake Isle of Innisfree", "Easter, 1916", "The Second Coming"],
+            image: "https://d.imgvision.net/irishcreativeproject/999f39ed-6813-4bf4-b8c2-ebd1a86ffd25.jpg"
+        }]
+    },
+    "Galway": {
+        count: 1,
+        figures: [{
+            name: "Lady Gregory",
+            dates: "1852 – 1932",
+            bio: "Co-founder of the Abbey Theatre and a key folklorist who drove the Irish Literary Revival.",
+            works: ["Cathleen Ni Houlihan", "The Rising of the Moon"],
+            image: "https://d.imgvision.net/irishcreativeproject/f1f28a8b-3959-401b-bb19-a96052b524fd.jpg"
+        }]
+    },
+    "Derry": {
+        count: 1,
+        figures: [{
+            name: "Brian Friel",
+            dates: "1929 – 2015",
+            bio: "Regarded as the 'Irish Chekhov,' he was a giant of English-language drama and founder of Field Day Theatre Co.",
+            works: ["Translations", "The Freedom of the City"],
+            image: "https://d.imgvision.net/irishcreativeproject/7c2cbae1-7304-49d4-b5e6-80e7249cb9fd.jpg"
+        }]
+    },
+    "Donegal": {
+        count: 1,
+        figures: [{
+            name: "Séamus Ó Grianna",
+            dates: "1889 – 1969",
+            bio: "A staunch defender of the Irish language who refused to publish in anything but traditional Gaelic type.",
+            works: ["Rann na Feirste"],
+            image: "https://d.imgvision.net/irishcreativeproject/7a778cc0-44e0-47d4-894c-48c124d8a40e.jpg"
+        }]
+    },
+    "Mayo": {
+        count: 1,
+        figures: [{
+            name: "Michael Davitt",
+            dates: "1846 – 1906",
+            bio: "Revolutionary and founder of the Land League; he pioneered the cooperation between physical force and political nationalism.",
+            works: ["The Fall of Feudalism in Ireland"],
+            image: "https://d.imgvision.net/irishcreativeproject/michael-davitt-portrait-circa-1878.jpg"
+        }]
+    },
+    "Kerry": {
+        count: 1,
+        figures: [{
+            name: "Daniel O'Connell",
+            dates: "1775 – 1847",
+            bio: "Known as 'The Liberator,' he achieved Catholic Emancipation through non-violent mass mobilization.",
+            works: ["Legal & Political Orations"],
+            image: "https://d.imgvision.net/irishcreativeproject/3dcdee9f-0889-4f8e-841f-6013261269aa.jpg"
+        }]
+    },
+    "Leitrim": {
+        count: 1,
+        figures: [{
+            name: "John McGahern",
+            dates: "1934 – 2006",
+            bio: "Hailed as the most important Irish novelist since Beckett, known for his masterful dissection of Irish rural life.",
+            works: ["Amongst Women", "The Barracks"],
+            image: "https://d.imgvision.net/irishcreativeproject/mcgahrn.jpg"
+        }]
+    },
+    "Roscommon": {
+        count: 1,
+        figures: [{
+            name: "Douglas Hyde",
+            dates: "1860 – 1949",
+            bio: "The first President of Ireland and a linguist who fought to de-Anglicise Irish culture.",
+            works: ["A Literary History of Ireland"],
+            image: "https://d.imgvision.net/irishcreativeproject/f7e551d6-825a-4a0a-b573-74caef994731.jpg"
+        }]
+    },
+    "Cork": {
+        count: 1,
+        figures: [{
+            name: "Thomas Crofton Croker",
+            dates: "1798 – 1854",
+            bio: "An antiquary whose collection of southern Irish folklore provided the foundation for the Literary Revival.",
+            works: ["Researches in the South of Ireland"],
+            image: "https://d.imgvision.net/irishcreativeproject/crofton-croker.jpg"
+        }]
+    },
+    "Longford": {
+        count: 1,
+        figures: [{
+            name: "Padraic Colum",
+            dates: "1881 – 1972",
+            bio: "A founder of the Abbey Theatre and a prolific collector of Irish folklore and children's stories.",
+            works: ["A Treasury of Irish Folklore", "The King of Ireland's Son"],
+            image: "https://d.imgvision.net/irishcreativeproject/Padraic-Colum.jpg"
+        }]
+    },
+    "Waterford": {
+        count: 1,
+        figures: [{
+            name: "Thomas Francis Meagher",
+            dates: "1823 – 1867",
+            bio: "Revolutionary leader who first introduced the Irish Tricolour flag and later became a US Civil War General.",
+            works: ["Speeches from the Dock"],
+            image: "https://d.imgvision.net/irishcreativeproject/Hotpot-12.jpg"
+        }]
+    },
+    "Westmeath": {
+        count: 1,
+        figures: [{
+            name: "Oliver Goldsmith",
+            dates: "1728 – 1774",
+            bio: "Versatile Georgian-era writer known for critiquing the decline of the Irish countryside under British rule.",
+            works: ["The Deserted Village", "She Stoops to Conquer"],
+            image: "https://d.imgvision.net/irishcreativeproject/0c3bf066-0855-461f-8bda-3a3f1fbceac0.jpg"
+        }]
+    },
+    "Wexford": {
+        count: 1,
+        figures: [{
+            name: "Thomas Cloney",
+            dates: "1774 – 1850",
+            bio: "A United Irishman and general during the 1798 Rebellion who survived to tell the tale in his memoirs.",
+            works: ["A Personal Narrative of 1798"],
+            image: "https://d.imgvision.net/irishcreativeproject/images.jpg"
+        }]
+    },
+    "Cavan": {
+        count: 1,
+        figures: [{
+            name: "Francis Ledwidge",
+            dates: "1887 – 1917",
+            bio: "The 'Poet of the Blackbirds,' a rural poet killed in WWI whose heart remained with the cause of Irish freedom.",
+            works: ["Songs of the Fields"],
+            image: "https://d.imgvision.net/irishcreativeproject/For_remembrance__soldier_poets_who_have_fallen_in_the_war__Adcock__1920_DJVU_pg_71.jpg"
+        }]
+    },
+    "Limerick": {
+        count: 1,
+        figures: [{
+            name: "Con Colbert",
+            dates: "1888 – 1916",
+            bio: "Executed leader of the Easter Rising and dedicated member of the Irish Republican Brotherhood.",
+            works: ["Revolutionary Writings"],
+            image: "https://d.imgvision.net/irishcreativeproject/4272_109003729342.jpg"
+        }]
+    },
+    "Monaghan": {
+        count: 1,
+        figures: [{
+            name: "Patrick Kavanagh",
+            dates: "1904 – 1967",
+            bio: "Poet who captured the raw reality of Irish peasant life, moving away from the romanticized versions of the past.",
+            works: ["The Great Hunger", "On Raglan Road"],
+            image: "https://d.imgvision.net/irishcreativeproject/483ef9ef-758c-46e4-a995-864dcf7b0514.jpg"
+        }]
+    },
+    "Fermanagh": {
+        count: 1,
+        figures: [{
+            name: "John Montague",
+            dates: "1929 – 2016",
+            bio: "A contemporary poetic giant whose work explored the 'rough field' of Northern Irish history.",
+            works: ["The Rough Field", "The Dead Kingdom"],
+            image: "https://d.imgvision.net/irishcreativeproject/1a7de221-d777-4012-8aed-a9f07ade62b1.jpg"
+        }]
+    },
+    "Tipperary": {
+        count: 1,
+        figures: [{
+            name: "John O'Leary",
+            dates: "1830 – 1907",
+            bio: "Fenian leader and mentor to Yeats; he famously said there are things a man must not do to save a nation.",
+            works: ["Recollections of Fenians and Fenianism"],
+            image: "https://d.imgvision.net/irishcreativeproject/images.jpg"
+        }]
+    },
+    "Tyrone": {
+        count: 1,
+        figures: [{
+            name: "Flann O'Brien",
+            dates: "1911 – 1966",
+            bio: "Master satirist and postmodern novelist, famous for his wit and linguistic playfulness.",
+            works: ["At Swim-Two-Birds", "The Third Policeman"],
+            image: "https://d.imgvision.net/irishcreativeproject/cropped_2_Flann_OBrien_via_RTE_press.jpg"
+        }]
+    },
+    "Kilkenny": {
+        count: 2,
+        figures: [
+            {
+                name: "John Banim",
+                dates: "1798 – 1842",
+                bio: "Known as the 'Scott of Ireland,' he co-wrote tales of the Irish peasantry that blended superstition with political pessimism.",
+                works: ["The O'Hara Family Tales", "The Celt's Paradise"],
+                image: "https://d.imgvision.net/irishcreativeproject/John_Banim.jpg"
+            },
+            {
+                name: "Michael Banim",
+                dates: "1796 – 1874",
+                bio: "Collaborator with his brother John, Michael provided the authentic detail of rural life that defined their joint literary success.",
+                works: ["The Croppy", "Father Connell"],
+                image: "https://d.imgvision.net/irishcreativeproject/Portrait_of_Michael_Banim_P1817.jpg"
+            }
+        ]
+    },
+    "Armagh": {
+        count: 1,
+        figures: [{
+            name: "John Hewitt",
+            dates: "1907 – 1987",
+            bio: "Prominent poet of the 'Ulster Regionalist' movement, advocating for a shared, non-sectarian culture.",
+            works: ["The Colony", "Once Alien Here"],
+            image: "https://d.imgvision.net/irishcreativeproject/John-Hewit-Poet-Sat-at-Desk-600x800.jpg"
+        }]
+    },
+    "Carlow": {
+        count: 1,
+        figures: [{
+            name: "James Fintan Lalor",
+            dates: "1807 – 1849",
+            bio: "A powerful revolutionary journalist whose writings on land reform deeply influenced later nationalists.",
+            works: ["The Faith of a Felon"],
+            image: "https://upload.wikimedia.org/wikipedia/commons/2/2a/James_Fintan_Lalor.jpg"
+        }]
+    },
+    "Wicklow": {
+        count: 1,
+        figures: [{
+            name: "Robert Erskine Childers",
+            dates: "1870 – 1922",
+            bio: "Author and nationalist executed during the Civil War; he famously smuggled arms for the Volunteers on his yacht.",
+            works: ["The Riddle of the Sands"],
+            image: "https://d.imgvision.net/irishcreativeproject/I0000f7vWpU0DVcA.jpg"
+        }]
+    },
+    "Kildare": {
+        count: 1,
+        figures: [{
+            name: "Teresa Brayton",
+            dates: "1868 – 1943",
+            bio: "A vital voice for the Irish diaspora in America, supporting the cause of freedom through her poetry.",
+            works: ["The Old Bog Road"],
+            image: "https://d.imgvision.net/irishcreativeproject/Brayton.jpg"
+        }]
+    },
+    "Down": {
+        count: 1,
+        figures: [{
+            name: "Seamus Heaney",
+            dates: "1939 – 2013",
+            bio: "Nobel Prize-winning poet who captured the soul of the Irish landscape and its complex history.",
+            works: ["Death of a Naturalist", "North"],
+            image: "https://d.imgvision.net/irishcreativeproject/SeamusHeaneyLowRes.jpg"
+        }]
+    },
+    "Antrim": {
+        count: 1,
+        figures: [{
+            name: "Louis MacNeice",
+            dates: "1907 – 1963",
+            bio: "A master of 20th-century poetry and BBC producer, known for exploring identity, belonging, and the introspective mind.",
+            works: ["Autumn Journal", "The Sunlight on the Garden", "Bagpipe Music"],
+            image: "https://d.imgvision.net/irishcreativeproject/images.jpg"
+        }]
+    },
+    "Offaly": {
+        count: 1,
+        figures: [{
+            name: "Alice Furlong",
+            dates: "1866 – 1946",
+            bio: "A founding member of Inghinidhe na hÉireann and a leading poetic voice for the nationalist spirit of the Midlands.",
+            works: ["Rays of Emerald Isle"],
+            image: "https://d.imgvision.net/irishcreativeproject/Alice_furlong-portrait.jpg"
+        }]
+    },
+    "Meath": {
+        count: 1,
+        figures: [{
+            name: "Geoffrey Keating",
+            dates: "1569 – 1644",
+            bio: "A monumental historian whose Gaelic writings preserved Irish mythology and history during the rise of English influence.",
+            works: ["Foras Feasa ar Éirinn (History of Ireland)"],
+            image: "https://d.imgvision.net/irishcreativeproject/1531652.jpg"
+        }]
+    },
+    "Laoighis": {
+        count: 1,
+        figures: [{
+            name: "Lucy Franks",
+            dates: "1878 – 1964",
+            bio: "A global advocate for rural life and a founding force behind the Associated Country Women of the World.",
+            works: ["ICA Revitalization Records"],
+            image: "" // No photo provided
+        }]
+    },
+    "Clare": {
+        count: 1,
+        figures: [{
+            name: "Dr. Kathleen Lynn",
+            dates: "1874 – 1955",
+            bio: "A physician and activist who served as Chief Medical Officer in the 1916 Rising and co-founded St. Ultan's Hospital.",
+            works: ["St. Ultan's Archive"],
+            image: "https://d.imgvision.net/irishcreativeproject/kathleen-lynn.jpg"
+        }]
+    }
+};
